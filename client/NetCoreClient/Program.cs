eb4223f7c449fd942a7b8cff5570c3bbafdde079
manuelterez <157379629+manuelterez@users.noreply.github.com>
2025-01-09 12:19:21 +0100

    Add files via upload

---
+++ client/NetCoreClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetCoreClient.Sensors;
+using NetCoreClient.Protocols;
+
+// define sensors
+List<ISensorInterface> sensors = new();
+sensors.Add(new VirtualWaterTempSensor());
+sensors.Add(new VirtualLightSensor());
+
+// define protocol
+// ProtocolInterface protocol = new Http("http://localhost:8011/water_coolers/123");
+// IProtocolInterface protocol = new Mqtt("test.mosquitto.org");
+IProtocolInterface protocol = new Coap("coap://192.168.100.115:5683"); // Utilizzo del protocollo CoAP
+
+// send data to server
+while (true)
+{
+    foreach (ISensorInterface sensor in sensors)
+    {
+        var sensorValue = sensor.ToJson();
+
+        protocol.Send(sensorValue, sensor.GetSlug());
+
+        Console.WriteLine("Data sent: " + sensorValue);
+
+        Thread.Sleep(5000);
+    }
+
+}
---
+++ client/NetCoreClient/Protocols/Coap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CoAP;
+using CoAP.Net;
+
+namespace NetCoreClient.Protocols
+{
+	internal class Coap : IProtocolInterface
+	{
+		private const string PATH_PREFIX = "casette/v1/id_1/sensori";
+		private readonly string endpoint;
+
+		public Coap(string endpoint)
+		{
+			this.endpoint = endpoint; // Esempio di endpoint: "coap://localhost"
+		}
+
+		public void Send(string data, string sensor)
+		{
+			try
+			{
+				// Costruisce l'URI completo per il sensore
+				string resourceUri = $"{endpoint}/{PATH_PREFIX}/{sensor}";
+
+				// Crea una richiesta CoAP POST
+				var request = new Request(Method.POST)
+				{
+					URI = new Uri(resourceUri),
+					PayloadString = data,
+					ContentType = MediaType.TextPlain // Imposta il tipo di contenuto su testo
+				};
+
+				Console.WriteLine($"[CoAP] Inviando dati: {data} a {resourceUri}");
+
+				// Invia la richiesta e ottiene la risposta (sincrono)
+				var response = request.Send();
+
+				// Gestisci la risposta
+				if (response != null)
+				{
+					// Se la libreria CoAP.NET utilizza un tipo di codice diverso, potrebbe essere qualcosa come response.Code
+					Console.WriteLine($"[CoAP] Risposta ricevuta: {response.Code}"); // Usa .Code per il codice di risposta
+				}
+				else
+				{
+					Console.WriteLine("[CoAP] Nessuna risposta ricevuta.");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Errore durante l'invio del messaggio CoAP: {ex.Message}");
+			}
+		}
+	}
+}
